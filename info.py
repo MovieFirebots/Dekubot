e8143f7fb9935b476ce3344a2be483bf1851cbb6
MovieFirebots <kesavansivakumar11@gmail.com>
2024-07-18 08:41:05 +0530

    Update info.py

--- info.py
+++ info.py
@@ -67,7 +67,7 @@ OWNER_USERNAME = environ.get('OWNER_USERNAME', 'SK_Deku') # owner username witho
 
 # Clone Information : If Clone Mode Is True Then Bot Clone Other Bots.
 CLONE_MODE = bool(environ.get('CLONE_MODE', True)) # Set True or False
-CLONE_DATABASE_URI = environ.get('CLONE_DATABASE_URI', "mongodb+srv://skdeku:skesavan7@cluster0.soycw3m.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0") # Necessary If clone mode is true
+CLONE_DATABASE_URI = environ.get('CLONE_DATABASE_URI', "mongodb+srv://DekuBotClones:Skesavan7@cluster0.8ize2jc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0") # Necessary If clone mode is true
 PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '+GpsERoY5iTJiYjM1') # Public Channel Username Without @ or without https://t.me/ and Bot Is Admin With Full Right.
 
 # Links
